049af8a3486944573ed44e34148cf3d49b562bb4
abdul-09 <abdul.9.hussein@gmail.com>
2024-02-15 16:28:33 +0300

    session in database

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -4,56 +4,30 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
-from api.v1.auth.auth import Auth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
 
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-
-
-if os.getenv('AUTH_TYPE') == 'session_db_auth':
-    from api.v1.auth.session_db_auth import SessionDBAuth
-    auth = SessionDBAuth()
-elif os.getenv('AUTH_TYPE') == 'session_exp_auth':
-    from api.v1.auth.session_exp_auth import SessionExpAuth
-    auth = SessionExpAuth()
-elif os.getenv('AUTH_TYPE') == 'session_auth':
-    from api.v1.auth.session_auth import SessionAuth
-    auth = SessionAuth()
-elif os.getenv('AUTH_TYPE') == 'basic_auth':
+if getenv('AUTH_TYPE') == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+elif getenv('AUTH_TYPE') == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-else:
-    auth = Auth()
-
-
-@app.before_request
-def check_auth():
-    """checks for user authentication for certain routes"""
-    if auth is None:
-        return
-    excluded_paths = [
-        '/api/v1/status/',
-        '/api/v1/unauthorized/',
-        '/api/v1/forbidden/',
-        '/api/v1/auth_session/login/'
-    ]
-    requires_auth = auth.require_auth(request.path, excluded_paths)
-    if requires_auth is False:
-        return
-    session_cookie = auth.session_cookie(request)
-    auth_header = auth.authorization_header(request)
-    if auth_header is None and session_cookie is None:
-        abort(401)
-    crnt_user = auth.current_user(request)
-    request.current_user = crnt_user
-    if crnt_user is None:
-        abort(403)
+elif getenv('AUTH_TYPE') == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
+elif getenv('AUTH_TYPE') == 'session_exp_auth':
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+    auth = SessionExpAuth()
+elif getenv('AUTH_TYPE') == 'session_db_auth':
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
 
 
 @app.errorhandler(404)
@@ -65,16 +39,31 @@ def not_found(error) -> str:
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """unauthorized handler
+    """ Request unauthorized
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """forbidden handler
+    """ Request forbidden
     """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request():
+    """ Before request
+    """
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/', '/api/v1/auth_session/login/']
+    if auth and auth.require_auth(request.path, excluded_paths):
+        if (not auth.authorization_header(request) and
+                not auth.session_cookie(request)):
+            abort(401)
+        if not auth.current_user(request):
+            abort(403)
+        request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -1,49 +1,46 @@
 #!/usr/bin/env python3
-"""Defining a class SessionExpAuth"""
+"""
+SessionExpAuth class to manage API authentication
+"""
 from api.v1.auth.session_auth import SessionAuth
+from os import getenv
 from datetime import datetime, timedelta
-import os
 
 
 class SessionExpAuth(SessionAuth):
-    """Class SessionExpAuth that inherits from SessionAuth"""
-    def __init__(self) -> None:
-        """Initializes a new SessionExpAuth"""
-        if os.getenv('SESSION_DURATION'):
-            self.session_duration = int(os.getenv('SESSION_DURATION'))
-        else:
+    """SessionExpAuth class to manage API authentication
+    """
+
+    def __init__(self):
+        """Initialize SessionExpAuth
+        """
+        try:
+            self.session_duration = int(getenv('SESSION_DURATION'))
+        except Exception:
             self.session_duration = 0
 
     def create_session(self, user_id=None):
-        """creates a session for a user"""
+        """Create session
+        """
         session_id = super().create_session(user_id)
-        if session_id is None:
-            return None
-
-        session_info = {
-            'user_id': user_id,
-            'created_at': datetime.now()
-        }
-        self.user_id_by_session_id[session_id] = session_info
-        return session_id
+        if session_id:
+            SessionAuth.user_id_by_session_id[session_id] = {
+                'user_id': user_id, 'created_at': datetime.now()}
+            return session_id
 
     def user_id_for_session_id(self, session_id=None):
-        """returns a user_id from a session_id"""
-        if session_id is None:
+        """Get user ID from session
+        """
+        if not session_id:
             return None
-
-        users_info = self.user_id_by_session_id.get(session_id)
-        if users_info is None:
+        session_dict = SessionExpAuth.user_id_by_session_id.get(session_id)
+        if not session_dict:
             return None
-
         if self.session_duration <= 0:
-            return users_info['user_id']
-
-        if 'created_at' not in users_info.keys():
+            return session_dict['user_id']
+        if 'created_at' not in session_dict:
             return None
-
-        if (users_info['created_at']
-                + timedelta(seconds=self.session_duration) < datetime.now()):
+        delta = timedelta(seconds=self.session_duration)
+        if session_dict['created_at'] + delta < datetime.now():
             return None
-
-        return users_info['user_id']
+        return session_dict['user_id']
